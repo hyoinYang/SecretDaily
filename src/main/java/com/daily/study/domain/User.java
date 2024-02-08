c328c2bab6090424e6f88ffbed7d88974ac30a4f
hyoinYang <hyoinyang02@gmail.com>
2024-02-08 17:51:23 +0900

    add SecurityConfig file

--- src/main/java/com/daily/study/domain/User.java
+++ src/main/java/com/daily/study/domain/User.java
@@ -1,28 +1,80 @@
-//package com.daily.study.entity;
-//
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@Builder
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class User {
-//    @Id
-//    private long id;
-//
-//    @Column(nullable = false, length = 100)
-//    private String username;
-//
-//    @Column(nullable = false, length = 100)
-//    private String password;
-//
-//    @Column(nullable = false)
-//    private boolean isPrivate;
-//}
+package com.daily.study.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name="users")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name="email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Builder
+    public User(String email, String password, String auth){
+        this.email = email;
+        this.password = password;
+    }
+
+    // 권한 반환
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+
+    // 사용자의 id(고유한 값)를 반환
+    @Override
+    public String getUsername(){
+        return email;
+    }
+
+    // 사용자의 패스워드 반환
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    // 계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // 만료되지 않았음
+    }
+
+    // 계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked(){
+        return true; // 잠금되지 않았음.
+    }
+
+    // 패스워드의 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true; // 만료되지 않았음.
+    }
+
+    // 계정의 사용 가부 여부 반환
+    @Override
+    public boolean isEnabled(){
+        return true; // 사용 가능함.
+    }
+
+}
---
+++ src/main/java/com/daily/study/service/UserDetailService.java
@@ -0,0 +1,19 @@
+package com.daily.study.service;
+
+import com.daily.study.domain.User;
+import com.daily.study.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(()->new IllegalArgumentException(email));
+    }
+}
