b10e14783f82c66fb34d874087a80cea06bfa2b0
hyoinYang <hyoinyang02@gmail.com>
2024-02-13 14:52:39 +0900

    add api and view in signup and login

---
+++ src/main/java/com/daily/study/service/UserService.java
@@ -0,0 +1,22 @@
+package com.daily.study.service;
+
+import com.daily.study.domain.User;
+import com.daily.study.dto.AddUserRequest;
+import com.daily.study.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
